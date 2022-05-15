fb6d7ebfccfedf920823af63546b16f31011a655
badsektor <gerald.villaran@lspu.edu.ph>
2022-05-15 16:30:02 +0800

    ml map clone

---
+++ Assets/Billboard.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+   public Transform cam;
+
+    private void Start() {
+        cam = Camera.main.transform;
+    }
+    void LateUpdate() {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CamerFollow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.125f;
